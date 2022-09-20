0bb12048ab2d4cedd3262040ca3293e088f94f59
gowripriya-2002 <107165064+gowripriya-2002@users.noreply.github.com>
2022-09-20 16:05:10 +0530

    Create asg_3.c

---
+++ Asg-3/asg_3.c
@@ -0,0 +1,25 @@
+#include<avr/io.h>
+#include<stdbool.h>
+int main(void){
+
+bool u,v,z,w,a;
+DDRB = 0b00001111; //8&9&10&11 as inputs
+PORTB= 0b11110000;
+DDRD = 0b00000100; //2as output
+
+//DDRB |= (1<<DDB5);
+
+
+while(1)
+   {
+	   u = (PINB & (1<<PINB0)) == (1<<PINB0);	
+	   v = (PINB & (1<<PINB1)) == (1<<PINB1);
+	   z = (PINB & (1<<PINB2)) == (1<<PINB2);
+	   w = (PINB & (1<<PINB3)) == (1<<PINB3);
+	   a=(u&&!z)||(!v&&z)||(!u&&z&&!w);
+	   PORTD |= (a<<2);
+
+   }
+
+	return 0;
+}
